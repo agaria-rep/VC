6c17e8d9cb1646ecb3b012ea9d7278eccc47cfcc
agaria-rep <96198356+agaria-rep@users.noreply.github.com>
2023-06-23 16:48:48 +0200

    RU Menu make

--- site/index.py
+++ site/index.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for, make_response
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    lang = request.cookies.get('lang')
+    if lang == None:
+        return redirect("/lang/ru/")
+    else:
+        return render_template("./"+lang+"/index.html")
+
+@app.route("/lang/<name>/")
+def lang(name):
+    response = make_response(redirect("/"))
+    response.set_cookie("lang", name)
+    
+    return response
+
+app.run(debug=True)
